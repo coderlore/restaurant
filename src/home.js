b1aad88e56fe0b489947bfd9510af62f3821eec7
coderlore <coderlore@gmail.com>
2021-08-31 22:26:23 -1000

    Fixed DOM issue with child element

--- src/home.js
+++ src/home.js
@@ -1,11 +1,11 @@
 function loadHome() {
-    const content = document.querySelector('#content');
-    const div = document.createElement('div');
+    let content = document.querySelector('#content');
+    content.id = 'content';
+
     let h1 = document.createElement('h1');
-
-    h1.innerHTML = '<p>Welcome to the restaurant!!</p>';
-
-    document.body.appendChild(h1);
+    h1.innerHTML = 'Come and try our tasty food!';
+    content.appendChild(h1);
+    document.body.appendChild(content);
 }
 
 export default loadHome
