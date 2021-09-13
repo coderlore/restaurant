e4c8f25ee5c4ecdd244ef6af04de124f38e765c5
coderlore <coderlore@gmail.com>
2021-09-13 08:29:29 -1000

    Added more content to homepage

--- src/home.js
+++ src/home.js
@@ -1,8 +1,11 @@
+import chef from '../asset/chef.jpeg';
+
 function loadHome() {  
     const content = document.querySelector('#content');
     content.id = 'content';
 
     const homePage = document.createElement('div');
+    homePage.id = 'home';
     homePage.setAttribute('data-tab-content', 'home')
     homePage.classList.add('show');
     homePage.classList.add('tab-content');
@@ -14,6 +17,17 @@ function loadHome() {
     const p = document.createElement('p');
     p.innerHTML = 'Tasty Hawaiian food at an affordable price';
     homePage.appendChild(p);
+
+    const chefImage = new Image();
+    chefImage.src = chef;
+    chefImage.alt = 'Chef Sherry';
+    chefImage.style.height = '300px';
+    chefImage.style.width = '300px';
+    homePage.appendChild(chefImage);
+
+    const message = document.createElement('p');
+    message.innerHTML = 'Come and visit us!';
+    homePage.appendChild(message);
 
     content.appendChild(homePage)
     document.body.appendChild(content);
