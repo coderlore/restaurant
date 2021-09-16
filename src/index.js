9a71088ce00d82797f3b5b271dace7f4b1adc4d3
coderlore <coderlore@gmail.com>
2021-09-16 07:57:43 -1000

    Removed HTML comments

--- src/index.js
+++ src/index.js
@@ -10,12 +10,3 @@ navbar()
 loadHome()
 loadMenu()
 contact()
-/*
-const content = document.querySelector('#content');
-const div = document.createElement('div');
-let h1 = document.createElement('h1');
-
-h1.innerHTML = '<p>Welcome to the restaurant!!</p>';
-
-document.body.appendChild(h1);
-*/
